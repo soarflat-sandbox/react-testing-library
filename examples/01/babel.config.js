2e801c1ea5a7ddf2c8d5c29521d64b1172fd3be2
soarflat <soarflat777@gmail.com>
2021-05-12 01:00:13 +0900

    TypeScript をテストできるようにした

--- examples/01/babel.config.js
+++ examples/01/babel.config.js
@@ -1,8 +1,13 @@
 const isTest = String(process.env.NODE_ENV) === 'test';
 
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
-    '@babel/preset-react',
-  ],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+    },
+  },
 };
--- examples/01/jest.config.js
+++ examples/01/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
 };
