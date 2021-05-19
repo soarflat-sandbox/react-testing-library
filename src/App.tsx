388a7cde6e3237c9505819a86d12a82edff0ceed
soarflat <soarflat777@gmail.com>
2021-05-19 17:42:26 +0900

    React Router を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { Counter } from '@/components/Counter'
 import { HiddenMessage } from '@/components/HiddenMessage'
@@ -10,6 +11,13 @@ const App: React.VFC = () => {
       <Counter />
       <h2>HiddenMessage Component</h2>
       <HiddenMessage>Message</HiddenMessage>
+      <h2>to register or login</h2>
+      <p>
+        <Link to="/register">Register</Link>
+      </p>
+      <p>
+        <Link to="/login">Login</Link>
+      </p>
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import App from '@/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/register">
+        <p>Register</p>
+      </Route>
+      <Route exact path="/login">
+        <p>Login</p>
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById('root'),
+)
